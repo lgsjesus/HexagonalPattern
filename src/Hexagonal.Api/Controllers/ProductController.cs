d3f62643601afd2eefe632193f35e695855d5050
Luiz Guilherme de Jesus <lguilherme.j@gmail.com>
2025-04-10 17:57:03 -0300

    feat: adjust apis routing

--- src/Hexagonal.Api/Controllers/ProductController.cs
+++ src/Hexagonal.Api/Controllers/ProductController.cs
@@ -7,18 +7,18 @@ namespace Hexagonal.Api.Controllers;
 [Route("api/[controller]")]
 public sealed class ProductController(IProductService service) : ApiBaseController
 {
-    [HttpGet]
+    [HttpGet("GetAll")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IEnumerable<ProductDto>> GetAllProduct()
         => (await service.GetAllProducts())
             .Select(c => (ProductDto) c).AsEnumerable();
-    [HttpGet]
+    [HttpGet("Get/{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ProductDto> GetProductById(Guid id)
         => await service.GetProductById(id);
-    [HttpGet]
+    [HttpPost("Create")]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ProductDto> CreateProduct(ProductDto dto)
--- src/Hexagonal.Api/Program.cs
+++ src/Hexagonal.Api/Program.cs
@@ -1,18 +1,25 @@
+using Hexagonal.Api.Controllers.Dtos;
 using Hexagonal.Domain.Entities.Products;
 using Hexagonal.EntityFramework;
 using Hexagonal.EntityFramework.Repositories;
 using Hexagonal.ProductService;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IProductService, ProductServices>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api Hexagonal", Version = "v1" });
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 if (string.IsNullOrEmpty(connectionString))
@@ -34,10 +41,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
+app.UseRouting();
 
 
 await app.RunAsync();
